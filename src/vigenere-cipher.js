bd636637a64e2bfccf5ee6527a86d3a497b72bf4
Alina <almaksimovic01@gmail.com>
2022-10-14 19:39:45 +0300

    feat: vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,19 +20,74 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
+  constructor(type) {
+    this.type = type;
+  }
+
   encrypt(message, key) {
     if (!key || !message) {
       throw new Error("Incorrect arguments!");
     }
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    alphabet = alphabet.split("");
     let cipherKey = key.toUpperCase().split("");
     let cipherMessage = message.toUpperCase().split("");
+    let offset;
+    let cipherStr = "";
+    let i = 0;
+
+    cipherMessage.forEach((x) => {
+      if (alphabet.indexOf(x) === -1) {
+        offset = alphabet.indexOf(cipherKey[i % cipherKey.length]);
+        cipherStr += x;
+        i = i - 1;
+      } else {
+        offset = alphabet.indexOf(cipherKey[i % cipherKey.length]);
+        cipherStr +=
+          alphabet.indexOf(x) + offset === 0
+            ? alphabet.join('').charAt(0)
+            : alphabet[(alphabet.indexOf(x) + offset) % alphabet.length];
+      }
+      i += 1;
+    });
+    if (this.type === false) {
+      return cipherStr.split("").reverse().join("");
+    }
+    return cipherStr;
   }
   decrypt(encryptedMessage, key) {
     if (!key || !encryptedMessage) {
       throw new Error("Incorrect arguments!");
     }
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    alphabet = alphabet.split("");
     let cipherKey = key.toUpperCase().split("");
     let cipherEncryptedMessage = encryptedMessage.toUpperCase().split("");
+    let offset;
+    let cipherStr = "";
+    let i = 0;
+
+    cipherEncryptedMessage.forEach((x) => {
+      if (alphabet.indexOf(x) === -1) {
+        offset = alphabet.indexOf(cipherKey[i % cipherKey.length]);
+        cipherStr += x;
+        i = i - 1;
+      } else {
+        offset = alphabet.indexOf(cipherKey[i % cipherKey.length]);
+        cipherStr +=
+          alphabet.indexOf(x) + offset === 0
+            ? alphabet.join('').charAt(0)
+            : alphabet[
+                (alphabet.indexOf(x) + alphabet.length - offset) %
+                  alphabet.length
+              ];
+      }
+      i += 1;
+    });
+    if (this.type === false) {
+      return cipherStr.split("").reverse().join("");
+    }
+    return cipherStr;
   }
 }
 module.exports = {
